34909285155a02b0e301144d8ade24245db0bd70
cas <cassaicus+git@gmail.com>
2026-05-30 16:40:43 +0900

    Initial Commit

---
+++ jumpapp/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  jumpapp
+//
+//  Created by ibis on 2026/05/30.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
